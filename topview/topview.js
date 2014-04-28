77529fa7db46870b354b0d2340335d7ae167d9b6
Alex Bolotov <oleksandr.bolotov@gmail.com>
2014-04-28 17:03:25 +0300

    + topview.html
    topview.html added

--- topview/topview.js
+++ topview/topview.js
@@ -1,13 +1,16 @@
 "use strict";
+
 var canv = document.getElementById('canv').getContext('2d');
 canv.fillStyle = "blue";
 canv.strokeStyle = "gray";
-mdl = {};
+// mdl = {};
+
 for(var x=4; x<100; x+=20){
-  for(var y=4; y<100; y+=10){
+    for(var y=4; y<100; y+=10){
 //    a = x; b = Math.sin(x) * 10;
 //    canv.strokeRect(x,y,x+2,y+2);
-    canv.strokeRect(x, y, 1, 1);
-  };
+      canv.strokeRect(x, y, 1, 1);
+    };
 };
-document.write("success");
+
+document.write("| success!");
